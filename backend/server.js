4d6a74fb56cfc1fb3fc4ee62bb42c5c672ee0248
VrushaliMore1203 <vrushali.more@iasssureit.com>
2026-05-03 21:44:17 +0530

    changed urls

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ 1. CORS FIRST (before everything)
 app.use(cors({
-    origin: "https://authenticator-app-indol.vercel.app",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
@@ -26,6 +26,7 @@ app.use('/api/auth', authRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: "https://authenticator-app-4j3l.onrender.com/api",
+    // NEXT_PUBLIC_API_URL: "http://localhost:5000/api",
   },
 };
 
